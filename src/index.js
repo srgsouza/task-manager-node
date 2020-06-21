0d2ad59fe2bb8b61faf6864204eecae8eab26e2a
Sergio Souza <emailsouza@gmail.com>
2020-06-21 10:02:26 -0600

    added welcome msg in home route

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+app.get('/', (req, res) => {
+    res.send('Welcome - This is a demo Node.js REST API application - /users and /tasks are live ');
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
